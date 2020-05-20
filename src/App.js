317d43595fc234419c1c91c43150a56cdf53416f
jcgonzaga <gonzaga.jc1993@gmail.com>
2020-05-21 02:46:03 +0900

    Mission 1: Design the UI using the given images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,356 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import classNames from "classnames";
+import { Arrow01, Arrow02, Calendar, Clip, MailSP, Search } from "./assets";
+import mockData from "./mock_data.json";
+import "./App.css";
 
-function App() {
+const MOMENT_FORMAT_DATE_TIME = "YYYY-MM-DD hh:mm";
+const MOMENT_FORMAT_DATE = "YYYY-MM-DD";
+
+const SearchField = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classNames("SearchWrapper", "SPWrapper")}>
+      <div className={"SearchContainer"}>
+        <img src={Calendar} alt="calendar" width={20} height={20} />
+        <input
+          className={"SearchField"}
+          type="text"
+          placeholder={"YYYY/MM/DD - YYYY/MM/DD"}
+        />
+      </div>
+      <div className={classNames("SearchContainer", "SearchIcon")}>
+        <img src={Search} alt="search" width={20} height={20} />
+      </div>
     </div>
   );
-}
+};
+
+const parseDate = (dateJson) => {
+  const dateMoment = moment(dateJson, MOMENT_FORMAT_DATE_TIME);
+  if (
+    dateMoment.format(MOMENT_FORMAT_DATE) ===
+    moment().format(MOMENT_FORMAT_DATE)
+  )
+    return dateMoment.format("HH:mm");
+  else if (dateMoment.isSame(moment(), "year"))
+    return dateMoment.format("MMM DD");
+  else return dateMoment.format("YYYY/MM/DD");
+};
+
+const sortMockData = (key, order = "asc") => {
+  return (a, b) => {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      return 0;
+    }
+    const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+    const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return order === "desc" ? comparison * -1 : comparison;
+  };
+};
+
+const App = () => {
+  const [mockDataParse, setMockDataParse] = useState(mockData);
+  const [sortHeader, setSortHeader] = useState("");
+
+  const onSortData = (key, order = "asc") => {
+    let updatedData = [...mockData];
+    let keyCheck = '';
+    if (key !== sortHeader) {
+      const tempMockData = [...mockDataParse];
+      updatedData = tempMockData.sort(sortMockData(key, order));
+      keyCheck = key
+    }
+    setMockDataParse(updatedData);
+    setSortHeader(keyCheck);
+  };
+
+  const [deviceType, setDeviceType] = useState("sp");
+
+  useEffect(() => {
+    if (!window.matchMedia) return;
+
+    const mediaQuerySP = window.matchMedia("(max-width: 743px)");
+
+    const updateMediaQuery = () => {
+      if (mediaQuerySP.matches) setDeviceType("sp");
+      else setDeviceType("pc/tl");
+    };
+
+    updateMediaQuery();
+    mediaQuerySP.addListener(updateMediaQuery);
+
+    return () => mediaQuerySP.removeListener(updateMediaQuery);
+  }, []);
+
+  return (
+    <div className="App">
+      <SearchField />
+      <span className={classNames("ResultText", "SPResultText")}>
+        Results: <span className={"ResultTotal"}>{mockData.length}</span>{" "}
+        mail(s)
+      </span>
+      <hr />
+      {mockData.length ? (
+        <div>
+          <div
+            className={classNames(
+              "SPTableWrapper",
+              "TableHeaderWrapper",
+              "TableRowHeader"
+            )}
+          >
+            <div
+              className={classNames(
+                deviceType === "sp" ? "RowMargin5" : "RowMargin15",
+                "RowHeader",
+                "FromRow",
+                sortHeader === "from" && "SortedRowHeader"
+              )}
+              onClick={() => onSortData("from")}
+            >
+              From
+              {sortHeader === "from" && (
+                <img
+                  className={"TableSortIcon"}
+                  src={Arrow01}
+                  alt="Sort"
+                  width={10}
+                  height={10}
+                />
+              )}
+            </div>
+            <div
+              className={classNames(
+                deviceType === "sp" ? "RowMargin5" : "RowMargin15",
+                "RowHeader",
+                "ToRow",
+                sortHeader === "to" && "SortedRowHeader"
+              )}
+              onClick={() => onSortData("to")}
+            >
+              To
+              {sortHeader === "to" && (
+                <img
+                  className={"TableSortIcon"}
+                  src={Arrow01}
+                  alt="Sort"
+                  width={10}
+                  height={10}
+                />
+              )}
+            </div>
+            <div
+              className={classNames(
+                deviceType === "sp" ? "RowMargin5" : "RowMargin15",
+                "RowHeader",
+                "SubjectRow",
+                sortHeader === "subject" && "SortedRowHeader"
+              )}
+              onClick={() => onSortData("subject")}
+            >
+              Subject
+              {sortHeader === "subject" && (
+                <img
+                  className={"TableSortIcon"}
+                  src={Arrow01}
+                  alt="Sort"
+                  width={10}
+                  height={10}
+                />
+              )}
+            </div>
+            <div
+              className={classNames(
+                deviceType !== "sp" && "RowHeader",
+                "DateRow",
+                sortHeader === "date" && "SortedRowHeader"
+              )}
+              onClick={() => onSortData("date", "desc")}
+            >
+              Date
+              {sortHeader === "date" && (
+                <img
+                  className={"TableSortIcon"}
+                  src={Arrow01}
+                  alt="Sort"
+                  width={10}
+                  height={10}
+                />
+              )}
+            </div>
+          </div>
+          {mockDataParse.map((item, idx) =>
+            deviceType !== "sp" ? (
+              <div
+                key={idx}
+                className={classNames("TableHeaderWrapper", "DataHover")}
+              >
+                <div
+                  className={classNames(
+                    "RowMargin15",
+                    "FromRow",
+                    "TextEllipsis"
+                  )}
+                >
+                  <span className={sortHeader === "from" && "SortedRowHeader"}>
+                    {item.from}
+                  </span>
+                </div>
+                <div
+                  className={classNames("RowMargin15", "ToRow", "TextEllipsis")}
+                >
+                  {item.to.length > 1 ? (
+                    <div className={"SpaceBetween"}>
+                      <div
+                        className={classNames(
+                          "TextEllipsis",
+                          sortHeader === "to" && "SortedRowHeader"
+                        )}
+                      >
+                        {item.to[0]}, ...
+                      </div>
+                      <div className={"MultiAddressee"}>
+                        +{item.to.length - 1}
+                      </div>
+                    </div>
+                  ) : (
+                    <span className={sortHeader === "to" && "SortedRowHeader"}>
+                      {item.to}
+                    </span>
+                  )}
+                </div>
+                <div
+                  className={classNames(
+                    "RowMargin15",
+                    "SubjectRow",
+                    "SpaceBetween"
+                  )}
+                >
+                  <div
+                    className={classNames(
+                      "RowMargin15",
+                      "SubjectRowText",
+                      "TextEllipsis",
+                      sortHeader === "subject" && "SortedRowHeader"
+                    )}
+                  >
+                    {item.subject}
+                  </div>
+                  {item.hasAttachment && (
+                    <img src={Clip} alt="Attachment" width={15} height={15} />
+                  )}
+                </div>
+                <div
+                  className={classNames(
+                    "DateRow",
+                    "TextEllipsis",
+                    sortHeader === "date" && "SortedRowHeader"
+                  )}
+                >
+                  {parseDate(item.date)}
+                </div>
+              </div>
+            ) : (
+              <div
+                key={idx}
+                className={classNames("HeaderWrapper", "DataHover")}
+              >
+                <div className={"SPDataWrapper"}>
+                  <img
+                    className={"RowMargin5"}
+                    src={MailSP}
+                    alt="MailSP"
+                    width={20}
+                    height={30}
+                  />
+                  <div className={"SPFromToWrapper"}>
+                    <div
+                      className={classNames("SpaceBetween", "MarginBottom5")}
+                    >
+                      <div
+                        className={classNames(
+                          "TextEllipsis",
+                          sortHeader === "from" && "SortedRowHeader"
+                        )}
+                      >
+                        {item.from}
+                      </div>
+                      <div className={"SpaceBetween"}>
+                        {item.hasAttachment && (
+                          <img
+                            className={"RowMargin5"}
+                            src={Clip}
+                            alt="Attachment"
+                            width={15}
+                            height={15}
+                          />
+                        )}
+                        <div
+                          className={classNames(
+                            "DateRow",
+                            "TextEllipsis",
+                            "RowMargin5",
+                            sortHeader === "date" && "SortedRowHeader"
+                          )}
+                        >
+                          {parseDate(item.date)}
+                        </div>
+                        <img src={Arrow02} alt="Arrow 2" width={7} height={7} />
+                      </div>
+                    </div>
+                    <div className={classNames("RowMargin15", "TextEllipsis")}>
+                      {item.to.length > 2 ? (
+                        <div className={"SpaceBetween"}>
+                          <div
+                            className={classNames(
+                              "TextEllipsis",
+                              sortHeader === "to" && "SortedRowHeader"
+                            )}
+                          >
+                            {item.to.slice(0, 2)}, ...
+                          </div>
+                          <div className={"MultiAddressee"}>
+                            +{item.to.length - 2}
+                          </div>
+                        </div>
+                      ) : (
+                        <span
+                          className={sortHeader === "to" && "SortedRowHeader"}
+                        >
+                          {item.to}
+                        </span>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={classNames(
+                    "SPSubjectText",
+                    "TextEllipsis",
+                    sortHeader === "subject" && "SortedRowHeader"
+                  )}
+                >
+                  {item.subject}
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      ) : (
+        <div className="ContentWrapper">
+          <img src={process.env.PUBLIC_URL + "/files/logo.png"} alt="logo" />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Arrow01} from './icon_arrow01.svg';
+export { default as Arrow02} from './icon_arrow02.svg';
+export { default as Calendar} from './icon_calender.svg';
+export { default as Clip} from './icon_clip.svg';
+export { default as MailSP} from './icon_mail_sp.svg';
+export { default as Search} from './icon_search.svg';
